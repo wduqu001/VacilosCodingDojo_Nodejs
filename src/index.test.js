60339ac15da24d1619af74002adc4560dba4720e
Willian Duque <wduqu001@gmail.com>
2019-08-25 19:11:43 -0300

    setup inicial com primeiro teste

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+
+describe('tests setup configuration', () => {
+    it('should be true', () => {
+        expect(true).toBeTruthy();
+    })
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
